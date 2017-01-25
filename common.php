e7933a6c738e0b3770a3079e05a0f58b9f54a557
s.savchenko <s.savchenko@outlook.com>
2017-01-25 20:22:58 +0700

    Common.

---
+++ common.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: savchenko
+ * Date: 25.01.17
+ * Time: 18:24
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once 'GoogleSheets.php';
+
+$config = require_once('config.php');
+
+session_start();
