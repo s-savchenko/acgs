59ea6fe96806b8f945322b16c9df59b2a141e972
s.savchenko <s.savchenko@outlook.com>
2017-01-25 22:53:23 +0700

    Config.

--- config_example.php
+++ config_example.php
@@ -9,5 +9,7 @@
 return [
     'googleAppName' => 'Google Sheets API PHP Quickstart',
     'googleCredentialsFile' => 'google_credentials.json',
-    'googleClientSecretFile' => 'client_secret.json'
+    'googleClientSecretFile' => 'client_secret.json',
+    'googleListId' => '1XwpioP4KyiFg9V5cQx5kIwuM1JidPgtGweKipzkPDHI',
+    'googleSheetRange' => 'Sheet1!B2'
 ];
